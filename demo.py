c8b7301d8b23d62c5dbcb28fd651c1f8cafe6024
Leonardo Zecchin <leonardoz@live.it>
2024-12-27 13:25:59 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -9,6 +9,29 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
+from dotenv import load_dotenv
+import os
+import boto3
+import pandas as pd
+from io import StringIO
+
+try:
+    load_dotenv()
+except Exception as e:
+    aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+    aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+    
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key
+)
+
+obj = s3.get_object(Bucket='my-nda-bucket', Key='data/myfile.csv')
+data = obj['Body'].read().decode('utf-8')
+
+df = pd.read_csv(StringIO(data))
+
 # -------------------------------------------------------
 # 1) Load the entire CSV once
 # -------------------------------------------------------
